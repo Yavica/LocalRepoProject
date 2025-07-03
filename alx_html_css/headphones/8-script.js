70fca8f132928a25ef990e0781b948148471c551
Jamie <jamiemaina06@gmail.com>
2025-07-03 08:03:53 +0300

    Delete alx_html_css directory

--- alx_html_css/headphones/8-script.js
+++
@@ -1,9 +0,0 @@
-// Wait for the DOM to fully load
-document.addEventListener('DOMContentLoaded', function () {
-  const hamburger = document.getElementById('hamburger');
-  const navMenu = document.getElementById('nav-menu');
-
-  hamburger.addEventListener('click', function () {
-    navMenu.classList.toggle('show');
-  });
-});
